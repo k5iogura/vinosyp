53d405af12c0aca8ed04a1a85cb45c08381ff24b
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-02-06 14:46:02 +0900

    revise

--- ie/YOLOv2/error.py
+++ ie/YOLOv2/error.py
@@ -20,4 +20,4 @@ print("%44s"%"std/max/min")
 
 for f in sys.argv[1:]:
     lines, std_l, max_l, min_l = readtxt(f)
-    print("%30s ="%(f),"%.8f %.8f %.8f"%(std_l, max_l, min_l))
+    print("%30s ="%(f[-30:]),"%.8f %.8f %.8f"%(std_l, max_l, min_l))
