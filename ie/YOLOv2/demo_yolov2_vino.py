aa79e1d681e0ac5e00bf1a44809e487f30a9a5b9
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-02-07 18:13:08 +0900

    revise

--- ie/YOLOv2/demo_yolov2_vino.py
+++ ie/YOLOv2/demo_yolov2_vino.py
@@ -82,12 +82,6 @@ def letterbox_image(im, w, h):
     embed_image(resized, boxed, int((w-new_w)//2), int((h-new_h)//2))
     return boxed
 
-def softmax(a):
-    exp_a = np.exp( a )
-    sum_exp_a = np.sum(exp_a)
-    y= exp_a / sum_exp_a
-    return y
-
 def IntersectionOverUnion(box_1, box_2):
     width_of_overlap_area  = min(box_1.xmax, box_2.xmax) - max(box_1.xmin, box_2.xmin)
     height_of_overlap_area = min(box_1.ymax, box_2.ymax) - max(box_1.ymin, box_2.ymin);
@@ -108,6 +102,29 @@ class DetectionObject:
         self.ymax = int(self.ymin + h * h_scale)
         self.class_id = class_id
         self.confidence = confidence
+
+def softmax(result, n, temp, stride, output):
+    largest = -10000.0
+    sums = 0.0
+    for i in range(n):
+        if result[i*stride] > largest:largest = result[i*stride]
+    for i in range(n):
+        e = np.exp(result[i*stride]/temp - largest/temp)
+        sums += e
+        output[i*stride] = e
+    for i in range(n):
+        output[i*stride] /= sums
+
+def softmax_cpu(result,n,batch,batch_offset,groups,group_offset,stride,temp,output):
+    for b in range(batch):
+        for g in range(groups):
+            res_out = softmax(
+                result[(b*batch_offset+g*group_offset):],
+                n,
+                temp,
+                stride,
+                output[(b*batch_offset+g*group_offset):]
+            )
 
 def EntryIndex(side_w, side_h, lcoords, lclasses, location, entry):
     n   = int(location / (side_w * side_h))
@@ -166,21 +183,32 @@ def ParseYOLOV2Output(
 def overlay_objects(image, objects):
     for obj in objects:
         confidence = obj.confidence
-        if obj.confidence < 0.5: continue
+        if obj.confidence <= 0.0: continue  # skip object rejected by NMS
         label = obj.class_id
-        # Drawing only objects when >confidence_threshold probability
         p1 = (obj.xmin, obj.ymin)
         p2 = (obj.xmax, obj.ymax)
-        cv2.rectangle(image, p1, p2, (255, 255, 255),1)
-        #cv2.putText(image, class_names[label], p1, cv::FONT_HERSHEY_TRIPLEX, 0.4, cv::Scalar(255, 0, 0), 0.2);
+        cv2.rectangle(image, p1, p2, (0, 0, 255),min(int(image.shape[2]//100),5))
+        cv2.putText(image,class_names[label],p1,cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
+
+def keep_aspect(image, new_h, new_w):
+    org_h, org_w = image.shape[:2]
+    if org_h > org_w:
+        w = int(new_w * org_h/org_w)
+        h = new_h
+    else:
+        w = new_w
+        h = int(new_h * org_w/org_w)
+    return cv2.resize(image,(w,h))
 
 args = argparse.ArgumentParser()
 args.add_argument("images", nargs='*', type=str)
 args.add_argument("-d", "--device"   , type=str, default="MYRIAD", help="Default MYRIAD or CPU")
 args.add_argument("-p", "--prefix", type=str, help="debug file prefix")
+args.add_argument("-s", "--softmax",action="store_true", help="aplly softmax")
+args.add_argument("-a", "--async",  action="store_true", help="aplly async IEngine")
 args = args.parse_args()
 
-input_image_size=(300,300)
+if args.softmax:print("Aplly softmax")
 
 data_type="FP16"
 if args.device == "CPU": data_type="FP32"
@@ -210,8 +238,7 @@ input_blob = next(iter(net.inputs))  #input_blob = 'data'
 out_blob   = next(iter(net.outputs)) #out_blob   = 'detection_out'
 model_n, model_c, model_h, model_w = net.inputs[input_blob].shape #Tool kit R4
 print("n/c/h/w (from xml)= %d %d %d %d"%(model_n, model_c, model_h, model_w))
-print("input_blob : out_blob =[",input_blob,":",out_blob,"]")
-print(net.outputs[out_blob].shape)
+print("input_blob : out_blob =[",input_blob,":",out_blob,"] ",net.outputs[out_blob].shape)
 
 del net
 
@@ -226,6 +253,7 @@ del net
 
 thresh_conf=0.69 # if 0.69 then can detect motorbike but 0.60 then detect person instead of motorbike
 thresh_conf=0.60 # But in YOLO-OpenVINO/YOLOv2/main.cpp thresh_conf is 0.5
+#thresh_conf=0.50 # But in YOLO-OpenVINO/YOLOv2/main.cpp thresh_conf is 0.5
 thresh_iou =0.45
 files=[]
 if len(args.images)>0:files = args.images
@@ -236,40 +264,56 @@ for f in files:
     frame_org = frame.copy()
     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     original_im_h, original_im_w = frame.shape[:2]
-
     frame =frame/255.0
-    #if args.prefix is not None:
-    #   save_as_txt(frame.transpose(2,0,1),args.prefix+"_im_"+args.device+".txt")
-
     frame =letterbox_image(frame, model_w, model_h)
-    #flat_ = frame.transpose(2,0,1).reshape(-1)
-    #if args.prefix is not None:save_as_txt(flat_,args.prefix+"_sized_"+args.device+".txt")
 
     #STEP-6
-    in_frame = cv2.resize(frame, (model_w, model_h))
-    in_frame = in_frame[np.newaxis,:,:,:]        # Add new axis as a batch dimension HWC NHWC
+    in_frame = frame[np.newaxis,:,:,:]        # Add new axis as a batch dimension HWC NHWC
     in_frame = in_frame.transpose((0, 3, 1, 2))  # Change data layout from NHWC to NCHW
-    print("in-frame",in_frame.shape)
+    print("in-frame.shape",in_frame.shape)
 
     start = time()
     #STEP-7
-    ASYNC = True
-    if ASYNC:
+    if args.async:
         exec_net.start_async(request_id=0, inputs={input_blob: in_frame})
     else:
         res = exec_net.infer(inputs={input_blob: in_frame})
 
     if exec_net.requests[0].wait(-1) == 0:
-        if ASYNC:
+        # result of inferrence have different formats btn async and sync execution
+        if args.async:
             res = exec_net.requests[0].outputs[out_blob]
-            print("ASYNC: res.shape",res.shape)
+            print("ASYNCRONOUS Inference: res.shape",res.shape)
             result = res.reshape(-1)
         else:
-            for outkey in res.keys(): print("outkey=",outkey)
+            for outkey in res.keys():
+                print("outkey=",outkey)
+                print("SYNCRONOUS Inference: res.shape",res[outkey].shape)
             result = res[outkey][0]
-            print("SYNC: result.shape",result.shape)
-        if args.prefix is not None:save_as_txt(result,args.prefix+"_region_"+args.device+".txt")
         sec = time()-start
+        if args.prefix is not None:save_as_txt(result,args.prefix+"_region_"+args.device+".txt")
+
+        # Apply softmax instead of Region layer
+        if args.softmax:
+            num = 5
+            coords = 4
+            classes=20
+            side_h = 13
+            side_w = 13
+            index = EntryIndex(side_h, side_w, 4, 0, 0, coords + 1)
+            softmax_cpu(
+                result[index:],
+                classes,
+                num,
+                int((side_h*side_w*num*(coords+1)+side_h*side_w*num*classes)/num),
+                side_w*side_h,
+                1,
+                side_w*side_h,
+                1,
+                result[index:]
+            )
+
+        # Pull objects from result
         objects = ParseYOLOV2Output(
             result,
         #    model_w,
@@ -289,7 +333,7 @@ for f in files:
                 if j<=i:continue
                 if IntersectionOverUnion(obj1, obj2) >= thresh_iou:
                     objects[i].confidence = 0.0
-        # draw
+        # Draw
         overlay_objects(frame_org, objects)
 
         print("frame_org.shape",frame_org.shape)
@@ -311,7 +355,7 @@ for f in files:
         print("error")
 
     #show result image
-    cv2.imshow('YOLOv2_demo',frame_org)
+    cv2.imshow('YOLOv2_demo',keep_aspect(frame_org, 480, 640))
     while True:
         key=cv2.waitKey(10)
         if key!=-1:
