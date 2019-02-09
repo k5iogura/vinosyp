d2bc8cee22d33f247f0412081288fb49802d3855
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-02-09 02:13:20 +0000

    revise

--- ie/YOLOv2tiny352x288-darknet-pb-ir/autostart_demo_ssd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /opt/intel/openvino/bin/setupvars.sh
-for i in `seq 1000`;do sleep 5;sudo sh -c "echo 0 > /sys/class/graphics/fb0/blank";done&
-sudo sh -c "echo 0 > /sys/class/graphics/fb0/blank"
-export DISPLAY=:0
-xhost +
-cd /home/pi/vinosyp/ie/YOLOv2tiny352x288-darknet-pb-ir
-xterm -geometry +10+10 -e "python3 tinyyolov2_demo_csi.py -crw 320 -crh 240"
