34540468c1534bdd9bb2a99b84208ccf4dc4dc8e
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-05-01 19:18:18 +0900

    revise

--- ie/MNIST-tf-pb-ir/mnist_predict.py
+++ ie/MNIST-tf-pb-ir/mnist_predict.py
@@ -13,7 +13,8 @@ import load
 mnist = load.mnist
 
 args = argparse.ArgumentParser()
-args.add_argument("images", nargs='*', type=str)
+args.add_argument("-n","--Nimages",    type=int, default=20*10,    help="predicted images")
+args.add_argument("-b","--batch_size", type=int, default=20,       help="net.batch_size")
 args.add_argument("-d", "--device"   , type=str, default="MYRIAD", help="Default MYRIAD or CPU")
 args = args.parse_args()
 
@@ -27,7 +28,7 @@ extension = "/inference_engine_samples/intel64/Release/lib/libcpu_extension.so"
 extension = os.environ['HOME']+extension
 if args.device == "CPU":plugin.add_cpu_extension(extension)
 net = IENetwork(model=model_xml, weights=model_bin)	# R5
-
+net.batch_size=args.batch_size
 print(model_bin, "on", args.device)
 exec_net = plugin.load(network=net, num_requests=1)
 
@@ -39,13 +40,9 @@ model_h = model_w = int(sqrt(model_h_x_model_w))
 print("input_blob shape(from xml)", net.inputs[input_blob].shape)
 print("name (input_blob : out_blob) =",input_blob,":",out_blob)
 
-del net
-
-Npred=40
-for idx,img in enumerate(mnist.test.images):
-    Npred-=1
-    if Npred<0:break
-    in_frame = img[np.newaxis,:]
+for idx in range(0,args.Nimages,net.batch_size):
+    img = mnist.test.images[idx:idx+net.batch_size]
+    in_frame = img
 
     start = time()
     exec_net.start_async(request_id=0, inputs={input_blob: in_frame})
@@ -53,14 +50,15 @@ for idx,img in enumerate(mnist.test.images):
     if exec_net.requests[0].wait(-1) == 0:
         sec = time() - start
         res = exec_net.requests[0].outputs[out_blob]
-        pred = np.argmax(res[0])
-        label= np.argmax(mnist.test.labels[idx])
-        est  = 'NG'
-        if pred==label:est = 'Pass'
-        print("predict/label = ( %d / %d ) %s"%(pred,label,est))
+        for i in range(net.batch_size):
+            pred = np.argmax(res[i])
+            label= np.argmax(mnist.test.labels[idx:idx+net.batch_size][i])
+            est  = 'NG'
+            if pred==label:est = 'Pass'
+            print("%d: predict/label = ( %d / %d ) %4s %.6f sec/image"%(i,pred,label,est,sec/net.batch_size))
     else:
         print("error")
-
+del net
 del exec_net
 del plugin
 sys.exit(-1)
